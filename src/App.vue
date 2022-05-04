15aa8939c64d8128235e2623cda6d0ee84b2d44e
Kamyab Nazari <kamyab.nz@gmail.com>
2022-05-04 23:02:36 +0200

    Navbar Vue Component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
+  <navbar></navbar>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  name: 'App',
+  components: { Navbar }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Srandom</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
