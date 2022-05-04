ff055bfb4428bc879535039b0140c097d13c4fc8
JonuziFlorina <86016416+JonuziFlorina@users.noreply.github.com>
2022-05-04 22:40:25 +0200

    Welcome to Srandom

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Srandom msg="Welcome to Srandom"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Srandom from '@/components/Srandom.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Srandom
   }
 }
 </script>
