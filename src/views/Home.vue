8b04bf780a56459465ac4774f9c14ffef6f3dec5
Kamyab Nazari <kamyab.nz@gmail.com>
2022-05-10 22:06:30 +0200

    Resizing homepage image.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" style="width: 15%; height: 15%">
     <WelcomeComponent msg="Welcome to Srandom"/>
   </div>
 </template>
