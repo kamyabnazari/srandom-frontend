d3fe9ea16d063b900d564a124e0c368e666734fb
JonuziFlorina <86016416+JonuziFlorina@users.noreply.github.com>
2022-06-27 10:45:10 +0200

    About us view

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1> About Us</h1>
+  <div class="card">
+    <div class="card-body">
+    <h1>You love to listen to songs but sometimes you can't decide what you want to hear? Then you are right with srandom! We have a solution for this. Furthermore we also offer you much more than that for example creating a playlist or favoriteing a song and much more. On srandom you will find over millions of songs. If you want to create your own playlist please create an account and see what awaits you :)</h1>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutUs',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
