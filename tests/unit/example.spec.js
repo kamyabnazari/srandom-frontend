fef391a179ca82881aa102005ed1cced8211525a
Kamyab Nazari <kamyab.nz@gmail.com>
2022-06-30 22:47:18 +0200

    Correct Test

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import Navbar from '@/components/NavbarComponent.vue'
+
+describe('NavarComponent.vue Test', () => {
+  it('check for correct component name', () => {
+    // render the component
+    const wrapper = shallowMount(Navbar)
+    // check the name of the component
+    expect(wrapper.vm.$options.name).toMatch('NavbarComponent')
+  })
+})
