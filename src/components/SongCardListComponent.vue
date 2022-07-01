07718953634d975cdbbb61f1fe2162395c903f07
Kamyab Nazari <kamyab.nz@gmail.com>
2022-07-01 08:26:03 +0200

    Fixed Column

--- src/components/SongCardListComponent.vue
+++ src/components/SongCardListComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-6 g-5">
+  <div class="row row-cols-auto gap-5 justify-content-center">
     <div class="col" v-for="song in songs" :key="song.id">
       <song-card :song="song"></song-card>
     </div>
